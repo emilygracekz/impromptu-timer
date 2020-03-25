b1c8bda534f20e09da01f8aeece3ddee9258b5df
Emily <57361805+emilygracekz@users.noreply.github.com>
2020-03-25 08:19:49 -0500

    Add files via upload
    Functional timer with NaN bug

--- impromptuTimer.js
+++ impromptuTimer.js
@@ -1,62 +1,63 @@
 let countdown;
 const timerDisplay = document.querySelector(".display__time-left");
-// const endTime = document.querySelector(".display__end-time");
-const start = document.getElementById("#button");
+const button = document.querySelector(".display button");
 const endpoint = "150000-quotes.p.rapidapi.com";
 
-function timer() {
-  const now = Date.now();
-  const then = now + 420 * 1000;
-  displayTimeLeft(then);
+function timer(seconds) {
+    //clear any existing items
+    clearInterval(countdown);
 
-  countdown = setInterval(() => {
-    const secondsLeft = Math.round((then - Date.now()) / 1000);
-    //check if timer at 0
-    if (secondsLeft < 0) {
-      clearInterval(countdown);
-      return;
-    }
+    const now = Date.now();
+    const then = now + seconds * 1000;
+    displayTimeLeft();
 
-    //display
-    displayTimeLeft(secondsLeft);
-    // console.log(secondsLeft);
-  }, 1000);
+    countdown = setInterval(() => {
+        const secondsLeft = Math.round((then - Date.now()) / 1000);
+        //check if timer at 0
+        if(secondsLeft < 0) {
+            clearInterval(countdown);
+            return;
+        }
+        //display
+        displayTimeLeft(secondsLeft);
+        console.log(secondsLeft);
+    }, 1000);
 }
 
-function displayTimeLeft() {
-  const minutes = Math.floor(420 / 60);
-  const remainderSeconds = 420 % 60;
-  const display = `${minutes}:${
-    remainderSeconds < 10 ? "0" : ""
-  }${remainderSeconds}`;
-  timerDisplay.textContent = display;
-  document.title = display;
+function displayTimeLeft(seconds) {
+    const minutes = Math.floor(seconds / 60);
+    const remainderSeconds = seconds % 60;
+    const display = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
+    timerDisplay.textContent = display;
+    document.title = display;
 }
-
 // function displayEndTime(timesetamp) {
 //     const end = new Date(timestamp);
 //     const hour = end.getHours();
 //     const minutes = end.getMinutes();
 //     endTime.text
 // }
+
 // function getQuote() {
-fetch("endpoint", {
-  method: "GET",
-  headers: {
-    "x-rapidapi-host": "150000-quotes.p.rapidapi.com",
-    "x-rapidapi-key": "463d16f422msh6c288922177a171p1532b2jsn9d2aaeca5188"
-  }
-})
-  .then(response => {
-    console.log(response);
-  })
-  .catch(err => {
-    console.log(err);
-  });
+// fetch("endpoint", {
+//   method: "GET",
+//   headers: {
+//     "x-rapidapi-host": "150000-quotes.p.rapidapi.com",
+//     "x-rapidapi-key": "463d16f422msh6c288922177a171p1532b2jsn9d2aaeca5188"
+//   }
+// })
+//   .then(response => {
+//     console.log(response);
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   });
 // }
 
-function sayHi() {
-  console.log("hi");
+function startTimer() {
+    const seconds = parseInt(this.dataset.time);
+    console.log(seconds);
+    timer(seconds);
 }
-timer();
-start.addEventListener("click", sayHi);
+
+button.addEventListener('click', startTimer);
